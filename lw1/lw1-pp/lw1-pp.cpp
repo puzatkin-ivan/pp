facc399316c27b6ef1fd54122f2fee9816729f58
puzatkin-ivan <puzatkin1998@gmail.com>
2019-06-14 00:21:28 +0300

    рефакторинг

--- lw1/lw1-pp/lw1-pp.cpp
+++ lw1/lw1-pp/lw1-pp.cpp
@@ -6,37 +6,12 @@
 #include <string>
 #include <vector>
 #include <windows.h>
-
-using Matrix = std::vector<std::vector<float>>;
-
-namespace
-{
-
-Matrix mainMatrix;
-
-int matrixSize;
-
-int threadNumber;
-
-static const double EPS = 1E-9;
-
-struct ThreadArguments
-{
-    std::istream* data;
-    Matrix* matrix;
-    int start, end, row, col, size;
-};
-
-}
-
-void MatrixOperations(std::istream& input, int threadCount);
-int Rank();
-int tryStringToInt(const std::string& str);
+#include "matrixFunction.h"
 
 int main(int argc, char* argv[])
 {
     HANDLE process = GetCurrentProcess();
-    SetProcessAffinityMask(process, 0b100);
+    SetProcessAffinityMask(process, 0b01);
 
     clock_t start = clock();
     if (argc != 3)
@@ -71,120 +46,3 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-int tryStringToInt(const std::string& str)
-{
-    int result = 0;
-    try
-    {
-        result = std::stoi(str, nullptr, 10);
-    }
-    catch (std::invalid_argument ex)
-    {
-        result = -1;
-    }
-    return result;
-}
-
-DWORD WINAPI GetMatrix(PVOID pvParam)
-{
-    auto threadArguments = (ThreadArguments*)pvParam;
-    float number;
-    std::vector<float> line;
-    for (int i = 0; i < threadArguments->size; i++)
-    {
-        line.clear();
-        for (int j = 0; j < threadArguments->size; j++)
-        {
-            (*(threadArguments->data)) >> number;
-            line.push_back(number);
-        }
-
-        (*(threadArguments->matrix)).push_back(line);
-    }
-
-    ExitThread(0);
-}
-
-DWORD WINAPI WorkMatrix(PVOID pvParam)
-{
-    auto threadArguments = static_cast<ThreadArguments*>(pvParam);
-    int j = threadArguments->row;
-    int i = threadArguments->col;
-
-    for (int k = threadArguments->start; k < threadArguments->end; ++k)
-    {
-        if (k != j && abs((*threadArguments->matrix)[k][i]) > EPS)
-        {
-            for (size_t p = i + 1; p < (*(threadArguments->matrix)).size(); ++p)
-            {
-                (*(threadArguments->matrix))[k][p] -= (*(threadArguments->matrix))[j][p] * (*(threadArguments->matrix))[k][i];
-            }
-        }
-    }
-
-    ExitThread(0);
-}
-
-void MatrixOperations(std::istream& input, int threads)
-{
-    input >> matrixSize;
-    threadNumber = threads > matrixSize ? matrixSize : threads;
-    auto* threadArguments = new ThreadArguments;
-    threadArguments->data = &input;
-    threadArguments->matrix = &mainMatrix;
-    threadArguments->size = matrixSize;
-    HANDLE* hTread = new HANDLE;
-
-    *hTread = CreateThread(NULL, 0, &GetMatrix, (PVOID)threadArguments, 0, NULL);
-    WaitForMultipleObjects(1, hTread, true, INFINITE);
-}
-
-int Rank()
-{
-    Matrix matrix(mainMatrix);
-    int rank = matrixSize;
-    std::vector<char> str(matrixSize);
-
-    for (int i = 0; i < matrixSize; i++)
-    {
-        int j;
-        for (j = 0; j < matrixSize; j++)
-        {
-            if ((abs(matrix[j][i]) > EPS) && !str[j]) { break; }
-        }
-
-        if (j == matrixSize)
-        {
-            --rank;
-        }
-        else
-        {
-            str[j] = true;
-            for (int p = i + 1; p < matrixSize; p++)
-            {
-                matrix[j][p] /= matrix[j][i];
-            }
-
-            int step = matrixSize / threadNumber;
-            int threadCounter = threadNumber;
-            HANDLE* hTread = new HANDLE[threadNumber];
-
-            for (int k = 0; threadCounter != 0; k += step)
-            {
-                --threadCounter;
-                auto* threadArguments = new ThreadArguments;
-                threadArguments->matrix = &matrix;
-                threadArguments->col = i;
-                threadArguments->row = j;
-                threadArguments->start = k;
-
-                threadArguments->end = (threadCounter != 0) ? k + step : matrixSize;
-
-                hTread[threadCounter] = CreateThread(NULL, 0, &WorkMatrix, (PVOID)threadArguments, 0, NULL);
-            }
-
-            WaitForMultipleObjects(threadNumber, hTread, true, INFINITE);
-        }
-    }
-    return rank;
-}
